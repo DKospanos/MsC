5df62246f34840213faae04b11a547a7c972eb25
DKospanos <152782375+DKospanos@users.noreply.github.com>
2024-01-06 19:11:33 +0200

    Create Test05.c

---
+++ Test05.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+
+
+
+
+{
+        char seq[ 1000 ];
+        int length;
+        int i;
+
+
+        while( scanf("%s", seq ) == 1)
+        {
+                length = strlen (seq);
+                printf("The lenght of the seq is %d\n", length);
+        }
